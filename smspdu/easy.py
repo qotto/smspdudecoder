5e787bf29240a6037e5bee534ee720298e6b4067
alexpirine <sacha@qotto.net>
2018-01-31 18:00:04 +0100

    Added easy handling of SMS PDU

---
+++ smspdu/easy.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+# Copyright (c) Qotto, 2018
+
+from .fields import SMSDeliver
+from io import StringIO
+
+from typing import Any, Dict
+
+__all__ = ['easy_sms']
+
+def easy_sms(data: str) -> Dict[str, Any]:
+    sms = SMSDeliver.decode(StringIO(data))
+    sender = sms['sender']['number']
+    if sms['sender']['toa']['ton'] == 'international':
+        sender = '+' + sender
+    date = sms['scts']
+    content = sms['user_data']['data']
+    header = sms['user_data'].get('header')
+    partial: Any = False
+    if header:
+        for element in header.get('elements', list()):
+            if element['iei'] in [0x00, 0x08]:
+                el_data = element['data']
+                partial = {
+                    'reference': f"{el_data['reference']}-{el_data['parts_count']}",
+                    'parts_count': el_data['parts_count'],
+                    'part_number': el_data['part_number'],
+                }
+    return {
+        'sender': sender,
+        'date': date,
+        'content': content,
+        'partial': partial,
+    }
