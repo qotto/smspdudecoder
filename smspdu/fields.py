507b1e56b636fcf1167c44106a291a9f5bb3641c
Alexandre Syenchuk <alexpirine@users.noreply.github.com>
2018-06-30 10:23:29 +0200

    Update fields.py

--- smspdu/fields.py
+++ smspdu/fields.py
@@ -4,7 +4,7 @@
 """
 TP-DU fields according to GSM 03.40.
 
-Unline elements, fields represent independent datagram chunks, and are decoded from a file-like object.xs
+Unlike elements, fields represent independent datagram chunks, and are decoded from a file-like object.xs
 
 Fields may contain one or multiple elements.
 """
