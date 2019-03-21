af0213085b943ace76ad9a2e161a9ef348bdc518
alexpirine <alex@pirine.fr>
2019-03-21 14:06:55 +0100

    Version bump to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name='smspdudecoder',
-        version='1.0.1',
+        version='1.1.0',
         url='https://github.com/qotto/smspdu',
         license='MIT',
         author='Alexandre Syenchuk',
