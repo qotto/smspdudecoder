8f27fba5e7c80addc45cf2e336c196343cd7569d
Sacha <alex@pirine.fr>
2022-02-07 14:28:29 +0100

    2.0.0 release

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Release notes
+
+## 2.0.0 (2022-02-07)
+
+- Package smspdu renamed to `smspdudecoder` for `consistency`.
+- Added tests for Python 3.7, 3.8, 3.9 and 3.10.
+
+## 1.2.1  (2019-07-22)
+
+- Added support for Python 3.6 and 3.7.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 if __name__ == '__main__':
     setup(
         name='smspdudecoder',
-        version='1.2.2',
+        version='2.0.0',
         url='https://github.com/qotto/smspdudecoder',
         license='MIT',
         author='Alexandre Syenchuk',
